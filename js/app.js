5c3cc5a7cae16168187dd84207d69386c253166d
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2020-12-03 22:15:01 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ let fetchData = fetchLink.then(function (data) {
 // -------------------------------------
 /*  Denna async function för att bladar kort utan att det blir bara två som har samma img på ett random sätt varje gång  */
 // -------------------------------------
-async function addPhotoToHtml() {
+async function addPhotoTArray() {
     let photoHtml, theImageRa, randomNumber;
     let ramdomImageArray = [];
     let duplicateForImageArray = []
@@ -90,7 +90,13 @@ async function addPhotoToHtml() {
 
 async function bildCard() {
 
-    await addPhotoToHtml()
+    await addPhotoTArray()
+    comparecartFunctoin()
+}
+// -------------------------------------
+
+let comparecartFunctoin = function () {
+
     let compareArray = []; /* för att jämfor varje gång länken för de två img som vi öpnnar i spelet */
     let imgHtmlArray = [];  /* För att samla alla imgages elementer i en array */
     let tempCompare = []; /* för att koden kommer ihåg vad hade vi för I nummer när vi öppnade kortet */
@@ -105,68 +111,83 @@ async function bildCard() {
 
     // ------------------------------------
 
-    /* föt syy skapa addEventListener för varje kort */
+
+
+
+
+
+
+    /* för att skapa addEventListener för varje kort */
     for (let i = 0; i <= 3; i++) {
-
-        console.log(i)
-        imgHtmlArray[i].addEventListener('click', muchCard)
-        function muchCard() {
-            console.log('addEventListener work')
-            if (compareArray.length < 2) {                  /* Vi kollar om vi har mindra än två kort som har vi öppnat */
-                console.log(i)
-
-                imgHtmlArray[i].src = ArrayforAllImg[i];                /* Då bytar vi src för img med den bilden vi fick från den array där vi har redan bilderna blandade */
-                tempCompare.push([i]);   /* 0 */                        /* För att hålla koll på vilket värde här (I) */
-                if (compareArray.indexOf(ArrayforAllImg[i]) === -1) {       /* Vi kollar om den url för bilden om vi fått in i compareArray finns redan i detta array eller inte */
-                    compareArray.push(ArrayforAllImg[i]);               /* Om inte så lägga vi till den till den array */
-
-
-                    if (compareArray.length == 2) {      /* Vi kollar om  compareArray har två olika kort med olika url så vänder vi kort igen om 1000s */
-                        console.log('dont Same')
-
-
-                        setTimeout(
-                            function () {
-                                imgHtmlArray[tempCompare[0]].src = "/img/memorycard.png"
-                                imgHtmlArray[tempCompare[1]].src = "/img/memorycard.png"
-                                tempCompare = []; /* Här behöver vi tomma array efter vi är klara med den */
-                                compareArray = [];  /* Här behöver vi tomma array efter vi är klara med den */
-                            }, 1000
-                        );
-
-                    }
-                } else if (compareArray.indexOf(ArrayforAllImg[i]) !== -1) {     /* Här kollar vi ifall den bilden vi fick från den array 
-                    där vi redan har bilderna blandade finns readan i array  och det betyder att vi har två kort då som har samma url*/
-
-                    compareArray.push(ArrayforAllImg[i]);     /*  här pushar vi bilden till compareArray igen  */
-                    console.log('the Same');
-                    removeLestn();                      /* Här vi kalar function för att removeEventListener för de img elementer som finns i imgHtmlArray
-                     med rätt index som vi kommer hämta från array som heter tempCompare och som spara var ligger i på varje gång vi öppnar någon kort  */
-                }
-
-
-
-                function removeLestn() {
-
-                    imgHtmlArray[tempCompare[0]].removeEventListener('click', muchCard);
-                    imgHtmlArray[tempCompare[1]].removeEventListener('click', muchCard);
-                    compareArray = [];    /* Här behöver vi tomma array efter vi är klara med den */
-                    tempCompare = [];     /* Här behöver vi tomma array efter vi är klara med den */
-                    console.log(i)
-
-                }
-            }
-        }
-
-
-
+        imgHtmlArray[i].addEventListener('click', matchakort)
     }
 
-}
+
+
+    function matchakort() {
+        console.log('addEventListener work')
+
+        for (let i = 0; i <= 3; i++) {
+
+    /* Vi kollar om vi har mindra än två kort som har vi öppnat */
+     /* Då bytar vi src för img med den bilden vi fick från den array där vi har redan bilderna blandade */
+            if (compareArray.length < 2) {     
+                imgHtmlArray[i].src = ArrayforAllImg[i];   
+                //             tempCompare.push([i]);   /* 0 */                        /* För att hålla koll på vilket värde här (I) */
+                //             if (compareArray.indexOf(ArrayforAllImg[i]) === -1) {       /* Vi kollar om den url för bilden om vi fått in i compareArray finns redan i detta array eller inte */
+                //                 compareArray.push(ArrayforAllImg[i]);               /* Om inte så lägga vi till den till den array */
+
+
+                //                 if (compareArray.length == 2) {      /* Vi kollar om  compareArray har två olika kort med olika url så vänder vi kort igen om 1000s */
+                //                     console.log('dont Same')
+                //                     setTimeout(
+                //                         function () {
+                //                             imgHtmlArray[tempCompare[0]].src = "/img/memorycard.png"
+                //                             imgHtmlArray[tempCompare[1]].src = "/img/memorycard.png"
+                //                             tempCompare = []; /* Här behöver vi tomma array efter vi är klara med den */
+                //                             compareArray = [];  /* Här behöver vi tomma array efter vi är klara med den */
+                //                         }, 1000
+                //                     );
+
+                //                 }
+                //             } else if (compareArray.indexOf(ArrayforAllImg[i]) !== -1) {     /* Här kollar vi ifall den bilden vi fick från den array 
+                //                     där vi redan har bilderna blandade finns readan i array  och det betyder att vi har två kort då som har samma url*/
+
+                //                 compareArray.push(ArrayforAllImg[i]);     /*  här pushar vi bilden till compareArray igen  */
+                //                 console.log('the Same');
+                //                 removeLestn();                      /* Här vi kalar function för att removeEventListener för de img elementer som finns i imgHtmlArray
+                //                      med rätt index som vi kommer hämta från array som heter tempCompare och som spara var ligger i på varje gång vi öppnar någon kort  */
+                //             }
 
 
 
-bildCard()
+                //             function removeLestn() {
+
+                //                 imgHtmlArray[tempCompare[0]].removeEventListener('click', matchakort);
+                //                 imgHtmlArray[tempCompare[1]].removeEventListener('click', matchakort);
+                //                 // imgHtmlArray[tempCompare[0]].remove();
+                //                 // imgHtmlArray[tempCompare[1]].remove();
+                //                 compareArray = [];    /* Här behöver vi tomma array efter vi är klara med den */
+                //                 tempCompare = [];     /* Här behöver vi tomma array efter vi är klara med den */
+                //                 console.log(i)
+
+                //             }
+                //         }
+
+                //     }
+
+
+
+
+
+
+                // }
+
+            }
+
+        }
+
+        bildCard()
 
 
 
