24b3312cdb65f289af4036edb7dc3f7978d523a3
Tara78 <70331037+Tara78@users.noreply.github.com>
2020-12-07 13:57:01 +0100

    Theme

--- js/app.js
+++ js/app.js
@@ -23,17 +23,21 @@ let unikIdforImg = [];
 
 // -------------------------------------
 
-// let searchWord = 'cats';
+let searchWord = 'cats';
 let searchWords = 'flowers';
-// let searchWord1='animals'
-let apiKey = '9588ff16cc05d4e98bcb23ab4b518b05'
-let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchWord-searchWords}&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1
+let searchWord1='birds';
+let searchWord2='animal'; 
+
+let apiKey = '9588ff16cc05d4e98bcb23ab4b518b05'; 
+
+let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchWord,searchWord1,searchWords,searchWord2}
+&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1
 `;
 
 
+// let url= `https://www.flickr.com/photos/{user-id}/albums/ -allalbums_key=${apiKey}&text=${searchWord}&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1`; 
 
-
-// const urls = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchWords}&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1
+// let urls = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchWords}&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1
 // `;
 
 
