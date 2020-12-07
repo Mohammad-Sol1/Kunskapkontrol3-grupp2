09c6902aae9303b074d521eb055d40aae9662d3e
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2020-12-07 17:59:10 +0100

    ämne och kort klar
    Färdig med att välja ämne och antal kort

--- js/app.js
+++ js/app.js
@@ -5,109 +5,105 @@ let bodyHtml = document.querySelector('body');
 let topSection = document.querySelector('.top-section')
 let secondSection = document.querySelector('.second-section')
 let cardContent = document.querySelector('.card-content')
-let winCardContent = document.querySelector('.win-card-content')
-let photContainer = document.querySelector('.container')
-let formFirstSide = document.querySelector(".first-section-form");
 let easyGame = document.getElementById('24K');
 let hardGame = document.getElementById('48K');
 let subjectValue = document.querySelector('.subject-value');
 let starrBtn = document.querySelector('.start-btn')
+let warningMessage = document.querySelector('.warning-message')
+let Gamesubject, GameLevel;
 
 
-choseLevelAndSubject()
-
-function choseLevelAndSubject() {
-    let GameLevel;
-    let Gamesubject;
-    starrBtn.addEventListener('click', (event) => {
+let choseLevelAndSubject = new Promise(function (resolve) {
+    starrBtn.addEventListener('click', function (event) {
         event.preventDefault();
-
         Gamesubject = (subjectValue.value).trim();
-
-
-        if (Gamesubject.length && (easyGame.checked ||hardGame.checked )) {
-            console.log('Vi har subjeckt and level ')
-
-            // ---------------------------------
+        if (Gamesubject.length && (easyGame.checked || hardGame.checked)) {
             if (easyGame.checked) {
                 GameLevel = easyGame.value;
-                console.log(GameLevel)
             }
             if (hardGame.checked) {
                 GameLevel = hardGame.value;
-                console.log(GameLevel)
             }
-
-            // ---------------------------------
-                subjectValue.value = ''
-                console.log(Gamesubject)
-        
+            subjectValue.value = ''
+            warningMessage.style.display = 'none';
+            resolve();
         }
+        else { warningMessage.style.display = 'block' };
+    }
+    )
+})
 
 
-
-
-
-    })
-
-
-
-
-
-
+async function startTheGame() {
+    await choseLevelAndSubject;
+    topSection.style.display= 'none';
+    secondSection.style.display= 'block';
 }
 
 
 
-// -------------------------------------
-
-let ArrayforAllImg = [];      /*  Den här array kommer vara det sista array där finns bilderna blandade redan på ett rätt sätt  */
-let unikIdforImg = [];
-
-// -------------------------------------
-
-let searchWord = 'cats';
-let apiKey = '9588ff16cc05d4e98bcb23ab4b518b05'
-let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchWord}&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1
-`;
 let arrayPhoto = [];
 
-// -------------------------------------
+let bildUrlforApi = async function () {
 
-/*  Här fitchar vi länken för flickr och skapa object for img länken */
-// -------------------------------------
+    let url;
+    await startTheGame()
 
+    let searchWord = Gamesubject;
+    let apiKey = '9588ff16cc05d4e98bcb23ab4b518b05'
+    url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchWord}&sort=relevance&safe_search=1&per_page=500&format=json&nojsoncallback=1
+ `;
+    let fetchLink = fetch(url).then(function (responsiv) {
+        return responsiv.json(url);
+    });
+    let fetchData = await fetchLink.then(function (data) {
 
-let fetchLink = fetch(url).then((responsiv) => {
-    return responsiv.json(url);
-});
-
-let fetchData = fetchLink.then(function (data) {
-
-    for (let i = 0; i <= 498; i++) {/*  498 - 8 yes*/
-        let photosData = function () {
-            this.PhotoServer = data.photos.photo[i].server;
-            this.PhotoId = data.photos.photo[i].id;
-            this.PhotoSecret = data.photos.photo[i].secret;
-            this.PhotoSize = 'q';
-            this.photoLink = `https://live.staticflickr.com/${this.PhotoServer}/${this.PhotoId}_${this.PhotoSecret}_${this.PhotoSize}.jpg`;
+        for (let i = 0; i <= 498; i++) {/*  498 - 8 yes*/
+            let photosData = function () {
+                this.PhotoServer = data.photos.photo[i].server;
+                this.PhotoId = data.photos.photo[i].id;
+                this.PhotoSecret = data.photos.photo[i].secret;
+                this.PhotoSize = 'q';
+                this.photoLink = `https://live.staticflickr.com/${this.PhotoServer}/${this.PhotoId}_${this.PhotoSecret}_${this.PhotoSize}.jpg`;
+            }
+            let thePhoto = new photosData().photoLink;
+            arrayPhoto.push(thePhoto);
         }
-        let thePhoto = new photosData().photoLink;
-        arrayPhoto.push(thePhoto);
-    }
-});
+    });
+
+}
+
+// -------------------------------------
 
 
+
+
+// // -------------------------------------
+
+// /*  Här fitchar vi länken för flickr och skapa object for img länken */
+// // -------------------------------------
+
+
+//
+
+
+/*  Den här array kommer vara det sista array där finns bilderna blandade redan på ett rätt sätt  */
+
+
+
+let ArrayforAllImg = [];
 // -------------------------------------
 /*  Denna async function för att bladar kort utan att det blir bara två som har samma img på ett random sätt varje gång  */
 // -------------------------------------
 async function addPhotoTArray() {
+    await bildUrlforApi();
     let photoHtml, theImageRa, randomNumber;
     let ramdomImageArray = []; /* Här ska vi samla 12 eller 24 unika bilder varja gång från data */
     let duplicateForImageArray = []
-    let kortantal = 12;/* 12 -4yes*/
-    await fetchLink;
-    while (ramdomImageArray.length < 12) {/* 12 -4yes */
+    let kortantal = (GameLevel/2);/* 12 -4yes*/
+    console.log(kortantal)
+    // await fetchLink;
+    while (ramdomImageArray.length < kortantal) {/* 12 -4yes */
         randomNumber = Math.floor(Math.random() * 499);/* 499 - 8 yes*/
         theImageRa = arrayPhoto[randomNumber];  /* Här hämtar vi en random bild av arrayPhoto som har 500 bilder */
         if (ramdomImageArray.indexOf(theImageRa) === -1) {
@@ -115,7 +111,6 @@ async function addPhotoTArray() {
             ArrayforAllImg.push(theImageRa);
             photoHtml = document.createElement('img');
             photoHtml.src = theImageRa;
-            // photContainer.appendChild(photoHtml);
         }
     }
     while (duplicateForImageArray.length < kortantal) {
@@ -126,11 +121,10 @@ async function addPhotoTArray() {
             ArrayforAllImg.push(theImageRa);
             photoHtml = document.createElement('img');
             photoHtml.src = theImageRa;
-            // photContainer.appendChild(photoHtml);
         }
     }
+    console.log(ArrayforAllImg)
 }
-
 
 // -------------------------------------
 
@@ -140,17 +134,13 @@ async function addPhotoTArray() {
 
 
 async function bildCard() {
-
-    await addPhotoTArray()
-    await skapaIDFörimg()
-
-    comparecartFunctoin()
-    // console.log(ArrayforAllImg);
+    await addPhotoTArray();
+    await skapaIDFörimg();
+    comparecartFunctoin();
 }
 
-
+let unikIdforImg = [];
 function skapaIDFörimg() {
-    // console.log(ArrayforAllImg)
     for (let i = 0; i < ArrayforAllImg.length; i++) {
         unikIdforImg.push(i + ArrayforAllImg[i])
     }
@@ -172,15 +162,14 @@ let comparecartFunctoin = function () {
 
 
     function skapaImgElementiArray() {
-        for (let i = 0; i <= 23; i++) {/*  23 - 7 yes*/     /* för att skapa alla kort och lägger till dem samma PNG img */
+        console.log(GameLevel)
+        for (let i = 0; i <= (GameLevel-1); i++) {/*  23 - 7 yes*/     /* för att skapa alla kort och lägger till dem samma PNG img */
             img = document.createElement('img'); /*  */
             img.src = "/img/memorycard.png"
             img.classList.add('img-element');
             cardContent.appendChild(img);
 
         }
-
-
     }
 
 
@@ -191,22 +180,13 @@ let comparecartFunctoin = function () {
     /* för att skapa addEventListener för varje kort */
 
     function skapaEvenetFörKort() {
-        for (let i = 0; i <= 23; i++) {   /* 7 yes */
+        for (let i = 0; i <= (GameLevel-1); i++) {   /* 7 yes */
             imgElement[i].addEventListener('click', matchakort)
-            function matchakort() {
-                // if (id.indexOf(i+ArrayforAllImg[i] !== -1) { 
-                // console.log('work event' + i)
-
-
-                // console.log(tempCompareForUnik)
+            function matchakort() {  
                 if (tempCompareForUnik.indexOf(unikIdforImg[i]) == -1) {
                     mindraÄnTvåBilder();
                     tempCompareForUnik.push(unikIdforImg[i])
-                    // console.log(tempCompareForUnik)
-
                 }
-
-
 
                 /* Vi kollar om vi har mindra än två url (kort) i compareArray som är tomt,
                 om de är mindre än två,
@@ -217,20 +197,13 @@ let comparecartFunctoin = function () {
                   annars om det nya url finns redan då betyder det att de två url (kort) är lika .
                   */
                 function mindraÄnTvåBilder() {
-                    // console.log(unikIdforImg)
-
-
                     if (compareArray.length < 2) {
                         if (compareArray.indexOf(ArrayforAllImg[i]) === -1) {
                             imgElement[i].src = ArrayforAllImg[i];
                             tempCompare.push([i]);     /* För att hålla koll på vilket värde här (I) */
-                            // console.log(tempCompare[0][0])
-                            // console.log(tempCompare[1][0])
-
                             compareArray.push(ArrayforAllImg[i]); /* */
                             // imgElement[tempCompare[0][0]].removeEventListener('click', matchakort)
                             tvåOlikaBilder();
-
                         } else {
                             tvålikaBilder()
                         }
@@ -240,8 +213,6 @@ let comparecartFunctoin = function () {
                 function tvåOlikaBilder() {
                     /* Vi kollar om  compareArray har två olika kort med olika url så vänder vi kort igen om 1000s */
                     if (compareArray.length == 2) {
-                        // console.log(tempCompare[0][0])
-                        // console.log(tempCompare[1][0])
                         console.log('dont Same')
                         setTimeout(
                             function () {
@@ -249,11 +220,7 @@ let comparecartFunctoin = function () {
                                 imgElement[tempCompare[1]].src = "/img/memorycard.png"
                                 tempCompare = [];
                                 compareArray = [];
-                                // console.log(tempCompareForUnik)
                                 tempCompareForUnik = []
-                                // console.log(tempCompareForUnik)
-
-
                                 /* Här behöver vi tomma array efter vi är klara med den */
                                 /* Här behöver vi tomma array efter vi är klara med den */
                             }, 2000
@@ -270,10 +237,7 @@ let comparecartFunctoin = function () {
                     compareArray.push(ArrayforAllImg[i]);
                     console.log('The same')
                     imgElement[i].src = ArrayforAllImg[i];
-                    // tempCompareForUnik = []
-
                     removeLestn();
-
                     function removeLestn() {
                         setTimeout(
                             function () {
@@ -302,7 +266,9 @@ let comparecartFunctoin = function () {
 }
 
 
-// bildCard()
+
+
+bildCard()
 
 
 
